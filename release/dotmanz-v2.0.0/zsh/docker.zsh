77e38cd9f330d2f91701d11fa34a55471a54f867
Ayush Poudel <ayushpoudel@usf.edu>
2025-05-26 00:08:18 -0400

    chore: remove manual release folder in favor of automated GitHub packaging

--- release/dotmanz-v2.0.0/zsh/docker.zsh
+++
@@ -1,7 +0,0 @@
-# -------------------------------
-# Docker / Compose Aliases
-# -------------------------------
-alias dcu='docker compose up -d'
-alias dcd='docker compose down'
-alias dcl='docker compose logs -f'
-alias dps='docker ps'
